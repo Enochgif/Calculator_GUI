9dbcb9c7ac57ae54f782e5d4226818556956e3d4
Prince David Ekpe <165416489+Princedavid12@users.noreply.github.com>
2025-08-12 16:03:52 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,11 +10,26 @@ e.grid(row=0, column=0,columnspan=3,padx=10, pady=10)
 def button_click(number):
 #    e.delete(0, END)
     current = e.get()
-    e.delete = (0, END)
+    e.delete(0, END)
     e.insert(0,str(current) + str(number))
 
 def button_clear():
-    e.delete = (0, END)
+    e.delete(0, END)
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+
 
 button_1 = Button(root, text="1", padx=40, pady=20,  command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20,  command=lambda: button_click(2))
@@ -26,8 +41,9 @@ button_7 = Button(root, text="7", padx=40, pady=20,  command=lambda: button_clic
 button_8 = Button(root, text="8", padx=40, pady=20,  command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20,  command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20,  command=lambda: button_click(0))
-button_ddd = Button(root, text="+", padx=39, pady=20,  command=lambda: button_click())
-button_equal = Button(root, text="=", padx=91, pady=20,  command=lambda: button_click())
+
+button_add = Button(root, text="+", padx=39, pady=20,  command=button_add)
+button_equal = Button(root, text="=", padx=91, pady=20,  command= button_equal)
 button_clear = Button(root, text="Clear", padx=71, pady=20,  command=button_clear)
 
 
@@ -52,3 +68,4 @@ button_equal.grid(row=5, column=1,columnspan=2)
 
 
 root.mainloop()
+
